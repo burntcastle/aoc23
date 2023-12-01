56582d4b439e93624540c9cfecb824cb30aea5fa
burntcastle <97998745+burntcastle@users.noreply.github.com>
2023-12-01 18:17:09 +0000

    Day one complete; updated interface

--- src/utils.rs
+++ src/utils.rs
@@ -11,28 +11,24 @@ pub enum ProblemInput<'a> {
     String(&'a str),
 }
 
+// this abstracts the input between strings (for testing) and files (for real)
 pub struct Input<'a>  {
     data: ProblemInput<'a> ,
 }
 impl Input<'_>  {
     pub fn new(input: ProblemInput) -> Input {
         Input { data: input }
-    }
+    }                 
 
     pub fn get_data(&self) -> Box<dyn BufRead + '_> {
         let result: Box<dyn BufRead> = match &self.data {
             ProblemInput::File(x) => {
                 let path = Path::new(x);
-
-                let display = path.display();
-                // Open the path in read-only mode, returns `io::Result<File>`
-
                 let file = match File::open(&path) {
-                    // The `description` method of `io::Error` returns a string that describes the error
-                    Err(why) => panic!("couldn't open {}: {}", display, Error::to_string(&why)),
+   
+                    Err(why) => panic!("couldn't open {}: {}", path.display(), Error::to_string(&why)),
                     Ok(file) => file,
                 };
-
                 let reader = BufReader::new(file);
                 Box::new(reader)
             }
