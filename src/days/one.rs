42f0e5e6e99e80cdf38d88b3bff2fb41a2049966
burntcastle <97998745+burntcastle@users.noreply.github.com>
2023-12-01 18:14:32 +0000

    Day one complete; updated interface

--- src/days/one.rs
+++ src/days/one.rs
@@ -1,27 +1,16 @@
-use core::num;
-use std::str;
 use std::io::prelude::*;
-use std::{io::{BufReader, Error}, path::Path, fs::File};
+use std::{
+    fs::File,
+    io::{BufReader, Error},
+    path::Path,
+};
 
-pub fn  part_one() -> u32 {
-    
-    let path = Path::new("./inputs/1/one");
-    let display = path.display();
+use crate::utils::Input;
 
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that describes the error
-        Err(why) => panic!("couldn't open {}: {}", display, Error::to_string(&why)),
-        Ok(file) => file,
-    };
-    
-    let reader = BufReader::new(file);
-    let lines = reader.lines(); 
+pub fn part_one(input: Input) -> u32 {
+    let lines = input.get_data().lines();
 
-    let numbers = vec!["zero","one","two","three","four","five","six","seven","eight","nine"];
-    let reverse_numbers: Vec<String> = numbers.clone().iter().map(|x| x.chars().rev().collect::<String>()).collect();
-
-    let mut total  = 0;
+    let mut total = 0;
     for l in lines {
         let line = l.unwrap();
         let mut char_vec: Vec<char> = (&line).chars().collect();
@@ -29,99 +18,153 @@ pub fn  part_one() -> u32 {
         let mut number_location = 0;
 
         for c in &char_vec {
-            match c.to_digit(10){
-                Some(x) => { first_number = x; break },
-                None => { number_location += 1; } ,
+            match c.to_digit(10) {
+                Some(x) => {
+                    first_number = x;
+                    break;
+                }
+                None => {
+                    number_location += 1;
+                }
             }
         }
-   
+
         let mut second_number = 0;
         number_location = 0;
         char_vec.reverse();
         for c in &char_vec {
-            match c.to_digit(10){
-                Some(x) => { second_number = x; break },
-                None => { number_location += 1; } ,
+            match c.to_digit(10) {
+                Some(x) => {
+                    second_number = x;
+                    break;
+                }
+                None => {
+                    number_location += 1;
+                }
             }
         }
-      
-        total += (first_number*10) + second_number;
+
+        total += (first_number * 10) + second_number;
+    }
+    total
+}
+
+pub fn part_two(input: Input) -> u32 {
+    let lines = input.get_data().lines();
+
+    let numbers = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let reverse_numbers: Vec<String> = numbers
+        .clone()
+        .iter()
+        .map(|x| x.chars().rev().collect::<String>())
+        .collect();
+    let mut total = 0;
+    for l in lines {
+        let line = l.unwrap();
+        let mut char_vec: Vec<char> = (&line).chars().collect();
+        let mut first_number = 0;
+        let mut number_location = 0;
+
+        for c in &char_vec {
+            match c.to_digit(10) {
+                Some(x) => {
+                    first_number = x;
+                    break;
+                }
+                None => {
+                    number_location += 1;
+                }
+            }
+        }
+        let mut i = 0;
+        for num in &numbers {
+            match line.find(num) {
+                Some(x) => {
+                    if x < number_location {
+                        first_number = i;
+                        number_location = x;
+                    }
+                }
+                None => {}
+            }
+            i += 1;
+        }
+
+        let mut second_number = 0;
+        number_location = 0;
+        char_vec.reverse();
+        for c in &char_vec {
+            match c.to_digit(10) {
+                Some(x) => {
+                    second_number = x;
+                    break;
+                }
+                None => {
+                    number_location += 1;
+                }
+            }
+        }
+        let line: String = char_vec.iter().collect();
+        let mut i = 0;
+        for num in &reverse_numbers {
+            let num = num.as_str();
+
+            match line.find(num) {
+                Some(x) => {
+                    if x < number_location {
+                        second_number = i;
+                        number_location = x
+                    }
+                }
+                None => {}
+            }
+            i += 1;
+        }
+        total += (first_number * 10) + second_number;
     }
     total
 }
 
 
 
-pub fn  part_two() -> u32 {
-    
-    let path = Path::new("./inputs/1/one");
-    let display = path.display();
+#[cfg(test)]
+mod tests {
+    use crate::utils::ProblemInput;
 
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that describes the error
-        Err(why) => panic!("couldn't open {}: {}", display, Error::to_string(&why)),
-        Ok(file) => file,
-    };
-    
-    let reader = BufReader::new(file);
-    let lines = reader.lines(); 
-
-    let numbers = vec!["zero","one","two","three","four","five","six","seven","eight","nine"];
-    let reverse_numbers: Vec<String> = numbers.clone().iter().map(|x| x.chars().rev().collect::<String>()).collect();
-    let mut total  = 0;
-    for l in lines {
-        let line = l.unwrap();
-        let mut char_vec: Vec<char> = (&line).chars().collect();
-        let mut first_number = 0;
-        let mut number_location = 0;
-
-        for c in &char_vec {
-            match c.to_digit(10){
-                Some(x) => { first_number = x; break },
-                None => { number_location += 1; } ,
-            }
-        }
-        let mut i = 0 ;
-        for num in &numbers {
-            match line.find(num){
-                Some(x) => {
-                    if x < number_location{
-                        first_number = i ;
-                        number_location = x;
-                    }                  
-                },
-                None => {}
-            }
-            i+=1;
-        }
-
-        let mut second_number = 0;
-        number_location = 0;
-        char_vec.reverse();
-        for c in &char_vec {
-            match c.to_digit(10){
-                Some(x) => { second_number = x; break },
-                None => { number_location += 1; } ,
-            }
-        }
-        let line: String = char_vec.iter().collect();
-        let mut i = 0 ;
-        for num in &reverse_numbers {
-            let num = num.as_str();
-            
-            match line.find(num){
-                Some(x) => {
-                    if x < number_location{
-                        second_number = i;
-                        number_location = x
-                    }                  
-                },
-                None => {}
-            }
-            i+=1;
-        }
-        total += (first_number*10) + second_number;
+    use super::*;
+    #[test]
+    fn part_one_test_digits() {
+        let input = "asdk1asfd2sdsdf4aa
+        asdk1asfd2sdsdf4aa";
+        let input = ProblemInput::String(input);
+        let result = part_one(Input::new(input));
+        assert_eq!(result,28);
     }
-    total
+    
+    #[test]
+    fn part_two_test_digits() {
+        let input = "asdk1asfd2sdsdf4aa
+        asdk3asfd2sdsdf2aa";
+        let input = ProblemInput::String(input);
+        let result = part_two(Input::new(input));
+        assert_eq!(result,46);
+    }
+    #[test]
+    fn part_two_test_words() {
+        let input = "athreesdk1asfd2sdsdf4sixzaa
+        dfasfivelaksjdasthreeasdasoneasd";
+        let input = ProblemInput::String(input);
+        let result = part_two(Input::new(input));
+        assert_eq!(result,87);
+    }
+    #[test]
+    fn part_two_test_mixed() {
+        let input = "asdninek1asfd2sdsdf4aa
+        asdddzerosdfj4alsasfdl1lsdd";
+        let input = ProblemInput::String(input);
+        let result = part_two(Input::new(input));
+        assert_eq!(result,95);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,36 @@
 use std::io;
 
+use crate::utils::{Input, ProblemInput};
+
 mod days;
+mod utils;
 
 fn read_i32() -> i32 {
-    
     let line = io::stdin().lines().next().unwrap().unwrap();
     line.parse().unwrap_or(-1)
 }
 
 fn main() {
-    loop{
-    println!("Enter day (0 to quit):");
-    let day = read_i32();
-    match day {
-        0 => {
-            println!("Exiting...");
-            break;
-        },
-        1=>{
-            println!("Day {}",day);
-            let result = days::one::part_one();
-            println!("Part One: {}",result);
-            let result = days::one::part_two();
-            println!("Part Two: {}",result);
-        },
-        _=>{
-            println!("Invalid Day")
+    loop {
+        println!("Enter day (0 to quit):");
+        let day = read_i32();
+        match day {
+            0 => {
+                println!("Exiting...");
+                break;
+            }
+            1 => {
+                let path = "./inputs/1/one";
+                let input = ProblemInput::File(path);
+                let result = days::one::part_one(Input::new(input));
+                println!("Part One: {}", result);
+                let result = days::one::part_two(Input::new(input));
+                println!("Part Two: {}", result);
+            }
+            _ => {
+                println!("Invalid Day")
+            }
         }
+        println!();
     }
-    println!();
-}
-
-    
-
-
 }
