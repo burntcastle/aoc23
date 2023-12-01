37cf8ec00408317b60ddc28db56362bfc276716b
burntcastle <97998745+burntcastle@users.noreply.github.com>
2023-12-01 17:36:15 +0000

    Day one complete; updated interface

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+use std::io;
+
 mod days;
 
+fn read_i32() -> i32 {
+    
+    let line = io::stdin().lines().next().unwrap().unwrap();
+    line.parse().unwrap_or(-1)
+}
+
 fn main() {
-    println!("Day One:");
-    let result = days::one::part_one();
-    println!("Part One:{}",result);
-    let result = days::one::part_two();
-    println!("Part Two:{}",result);
+    loop{
+    println!("Enter day (0 to quit):");
+    let day = read_i32();
+    match day {
+        0 => {
+            println!("Exiting...");
+            break;
+        },
+        1=>{
+            println!("Day {}",day);
+            let result = days::one::part_one();
+            println!("Part One: {}",result);
+            let result = days::one::part_two();
+            println!("Part Two: {}",result);
+        },
+        _=>{
+            println!("Invalid Day")
+        }
+    }
+    println!();
+}
+
+    
+
+
 }
